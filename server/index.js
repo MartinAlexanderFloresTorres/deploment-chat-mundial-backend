9d73ec7396e6755e6aaf137e3bceb881f89451d5
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-08-04 18:14:56 -0500

    version 1

--- server/index.js
+++ server/index.js
@@ -4,14 +4,16 @@ import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
 import { PORT } from "./config.js";
+let activos = 0;
 
 const app = express();
 const server = http.createServer(app);
 
 //=============== Permitir coneciones de este frontend ===============//
+/* origin: "http://localhost:3000", */
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-mundial.netlify.app",
   },
 });
 
@@ -23,6 +25,20 @@ io.on("connection", (socket) => {
       body: e,
       id: socket.id,
     });
+  });
+  activos += 1;
+
+  setInterval(() => {
+    socket.emit("actualizar", activos);
+  }, 1000);
+
+  socket.on("disconnect", () => {
+    if (activos >= 0) {
+      activos -= 1;
+    }
+    setInterval(() => {
+      socket.emit("actualizar", activos);
+    }, 1000);
   });
 });
 
